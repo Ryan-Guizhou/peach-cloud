09846d607001927f689c3205dba3b197c41beb93
Ryan <103166889+Ryan-Guizhou@users.noreply.github.com>
2026-01-18 20:16:26 +0800

    feat 引用用户服务的feignclint  并扫描对应的包 测试调用结果

--- peach-monitor/peach-monitor-launch/src/main/java/com/peach/monitor/launch/PeachMonitorApplication.java
+++ peach-monitor/peach-monitor-launch/src/main/java/com/peach/monitor/launch/PeachMonitorApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableDiscoveryClient
 @ComponentScan("com.peach")
 @SpringBootApplication
+@EnableFeignClients({"com.peach.userservice.openfeign"})
 public class PeachMonitorApplication {
 
     public static void main(String[] args) {
--- peach-monitor/peach-monitor-rest/src/main/java/com/peach/monitor/rest/MonitorController.java
+++ peach-monitor/peach-monitor-rest/src/main/java/com/peach/monitor/rest/MonitorController.java
@@ -1,11 +1,16 @@
 package com.peach.monitor.rest;
 
 
+import com.peach.common.response.Response;
 import com.peach.monitor.entity.UserDTO;
+import com.peach.userservice.openfeign.UserFeignClient;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @Author Mr Shu
@@ -16,6 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/monitor")
 public class MonitorController {
 
+    @Resource
+    private UserFeignClient userFeignClient;
+
     @GetMapping("/{id}")
     public UserDTO index(@PathVariable("id") String id) {
         UserDTO userDTO = new UserDTO();
@@ -23,5 +31,19 @@ public class MonitorController {
         userDTO.setName("Peach");
         userDTO.setEmail("huanhuanshu48@gmail.com");
         return userDTO;
+    }
+
+    @Operation(summary = "获取路由信息")
+    @GetMapping("/routerInfo/{routerId}")
+    public Response routerInfo(@PathVariable("routerId") String routerId) {
+        Response routerInfo = userFeignClient.getRouterInfo(routerId);
+        return routerInfo;
+    }
+
+    @Operation(summary = "获取路由信息")
+    @GetMapping("/roleInfo/{roleId}")
+    public Response roleInfo(@PathVariable("roleId") String roleId) {
+        Response routerInfo = userFeignClient.getRoleInfo(roleId);
+        return routerInfo;
     }
 }
